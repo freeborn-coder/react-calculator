62b2e37cfda7cf091b76ef3f9c41fe760a56f9ec
freeborn-coder <freebornganani@gmail.com>
2021-10-29 06:33:51 +0100

    refactored button container, improved calculator functionality

--- src/components/ButtonsContainer.js
+++ src/components/ButtonsContainer.js
@@ -1,31 +1,29 @@
-import { OperandButton, OperatorButton } from "./Buttons";
-
-function ButtonsContainer(props){
+export function ButtonsContainer(props){
 
     const nums = [7,8,9,4,5,6,1,2,3];
 
     return (
         <div className="h-3/5 bg-[#FBFBFB] rounded-2xl p-4 text-center">
-                <div className="grid grid-cols-4 grid-rows-5 gap-5 font-bold">
-                    <OperatorButton onClick={props.actions.clearInput} value="c"/>
-                    <OperatorButton value="%"/>
-                    <OperatorButton value="&#8592;"/>
-                    <OperatorButton value="&#xf7;"/>
-                    <div className="col-span-3 row-span-3">
-                        <div className="grid grid-cols-3">
-                            {nums.map(num => <OperandButton onClick={props.actions.setInput} value={num} key={num} />)}
-                            <OperatorButton value="+/-"/>
-                            <OperandButton onClick={props.actions.setInput} value={0} />
-                            <OperandButton value='.' onClick={props.actions.setInput} />
-                        </div>
+            <div className="grid grid-cols-4 grid-rows-5 gap-5 font-bold">
+                <button className="operator-btn" onClick={props.actions.clearInput}>c</button>
+                <button className="operator-btn">%</button>
+                <button className="operator-btn" onClick={props.actions.backSpace}>&#9592;</button>
+                <button className="operator-btn" onClick={props.actions.setInput}>/</button>
+                
+                <div className="col-span-3 row-span-3">
+                    <div className="grid grid-cols-3">
+                        {nums.map(num => <button className="operand-btn" 
+                            onClick={props.actions.setInput} key={num}>{num}</button> )}
+                        <button className="operator-btn">+/-</button>
+                        <button className="operand-btn" onClick={props.actions.setInput}>0</button>
+                        <button className="operator-btn" onClick={props.actions.setInput}>.</button>
                     </div>
-                    <button className="operator-btn" onClick={props.actions.setInput}>x</button>
-                    <button className="operator-btn" onClick={props.actions.setInput}>-</button>
-                    <button className="operator-btn" onClick={props.actions.setInput}>+</button>
-                    <button className="col-span-4 bg-[#5A7BFF] text-white rounded-xl font-bold text-xl" onClick={props.actions.setInput}>=</button>
                 </div>
+                <button className="operator-btn" onClick={props.actions.setInput}>x</button>
+                <button className="operator-btn" onClick={props.actions.setInput}>-</button>
+                <button className="operator-btn" onClick={props.actions.setInput}>+</button>
+                <button className="col-span-4 bg-[#5A7BFF] text-white rounded-xl font-bold text-xl" onClick={props.actions.setInput}>=</button>
             </div>
+        </div>
     );
 }
-
-export default ButtonsContainer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,34 @@
 import OutputScreen from "./OutputScreen";
-import ButtonsContainer from "./ButtonsContainer";
+import { ButtonsContainer } from "./ButtonsContainer";
 import { useState } from "react";
 
 export function Main(props){
 
     const [input, setInput] = useState('');
     const [output, setOutput] = useState(0);
+    const [lastInput,setLastInput] = useState(0);
 
 
     const actions = {
-        setInput:function(e){
+        setInput(e){
             let prev = input ?? '';
-            let prevStr = prev.toString();
-
-            let lastChar = prevStr.charAt(prevStr.length - 1);
 
             let val = e.target.innerText;
             let newInput;
             let res;
 
-            if(isNaN(val) || isNaN(lastChar)){
+            if(lastInput === '=') return;
+
+            if(isNaN(lastInput) && isNaN(val) && ! ['-','+'].includes(val)) return;
+
+            if(isNaN(val) || isNaN(lastInput)){
                 if(val === '='){
                     // compute answer
                     let stmt = prev.split(' ');
                     stmt = stmt.filter(elem => elem.trim().length > 0);
                     stmt = stmt.map(elem => isNaN(elem) ? elem : Number(elem));
-                    console.log(stmt);
+                    
+                    console.log(stmt.join(' '));
 
                     let [num1,operator,num2] = stmt;
 
@@ -33,7 +36,9 @@ export function Main(props){
                         case '+': res = num1 + num2; break;
                         case 'x': res = num1 * num2; break;
                         case '-': res = num1 - num2; break;
-                        default: res = num1/num2;
+                        case '/': res = num1 / num2; break;
+                        default:
+
                     }
                     
                 }
@@ -41,13 +46,21 @@ export function Main(props){
             }else{
                 newInput = prev + val;
             }
+
+            setLastInput(val);
             setInput(newInput);
             if(res) setOutput(res);
 
         },
-        clearInput:function(){
+        clearInput(){
            setInput(null);
+           setLastInput('');
            setOutput(0);
+        },
+        backSpace(){
+            let previousVal = input.trim();
+            setInput(previousVal.substring(0,previousVal.length - 1));
+            setLastInput(previousVal.substring(0,previousVal.length - 2));
         }
     }
     
